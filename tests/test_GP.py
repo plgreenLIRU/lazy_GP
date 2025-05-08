9bb4c70b6422c367446b35a18725caeec5b8b006
Pete Green <42935860+plgreenLIRU@users.noreply.github.com>
2025-05-08 14:20:39 +0000

    added new test

--- tests/test_GP.py
+++ tests/test_GP.py
@@ -14,9 +14,37 @@ def test_1D_regression():
 
     # Fit model
     m = GP()
-    m.set_hyperparameters(X=X, y=y, theta=1, sigma=0.1)
+    theta = np.array([1])
+    m.set_hyperparameters(X=X, y=y, theta=theta, sigma=0.1)
     y_star_pred = m.predict(X_star)
 
     # Check mse
     mse = mean_squared_error(y_star, y_star_pred)
     assert mse < 0.005
+
+def test_2D_regression():
+
+    # Define a 2D nonlinear function
+    def nonlinear_function(x):
+        return np.sin(x[:, 0]) * np.cos(x[:, 1])
+
+    # Generate input data
+    np.random.seed(42)
+    N = 500
+    X = np.random.uniform(-3, 3, (N, 2))
+    X_star = np.random.uniform(-3, 3, (100, 2))
+
+    # Generate noisy observations
+    sigma = 0.001
+    y = nonlinear_function(X) + sigma * np.random.randn(len(X))
+    y_star = nonlinear_function(X_star) + sigma * np.random.randn(len(X_star))
+
+    # Initialise model
+    m = GP()
+    theta = np.array([1., 1.])
+    m.set_hyperparameters(X, y, theta=theta, sigma=sigma)
+
+    # Predictions
+    y_star_pred = m.predict(X_star)
+    mse = mean_squared_error(y_star, y_star_pred)
+    assert mse < 0.002
