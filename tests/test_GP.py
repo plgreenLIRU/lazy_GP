369b8155e1491cb2b0780b0dbff3797691cb1c58
Pete Green <42935860+plgreenLIRU@users.noreply.github.com>
2025-05-14 09:58:25 +0000

    updating tests

--- tests/test_GP.py
+++ tests/test_GP.py
@@ -101,6 +101,6 @@ def test_tr_invK_dK():
     K, C, inv_C, dK_dtheta = gp._find_exact_matrices(X=X, theta=theta, sigma=sigma, d_dash=d_dash)
 
     exact_term = np.trace(inv_C @ dK_dtheta)
-    mc_estimate = gp._tr_invK_dK(X=X, theta=theta, sigma=sigma, d_dash=d_dash, S=10)
+    mc_estimate = gp._tr_invK_dK(X=X, theta=theta, sigma=sigma, d_dash=d_dash)
 
     assert np.allclose(exact_term, mc_estimate, rtol=0.05)
