ae96624197aac9065e489f41c3d671d90ecc6b99
plgreenLIRU <p.l.green@liverpool.ac.uk>
2025-05-15 09:39:19 +0100

    train method for ARGP

--- lazy_GP/ARGP.py
+++ lazy_GP/ARGP.py
@@ -1,38 +1,53 @@
 from .GP import GP
 import numpy as np
+import time
 
 class ARGP(GP):
 
-    def _prepare_arx_data(self, X, Y, N_AR):
+    def _prepare_arx_data(self, X, y, N_AR):
         """
         Prepares the ARX (Auto-Regressive with eXogenous inputs) data matrix.
 
         Parameters:
         X (numpy.ndarray): Exogenous input data of shape (N, D).
-        Y (numpy.ndarray): Target data of shape (N,).
+        y (numpy.ndarray): Target data of shape (N,).
         N_AR (int): autoregressive order
 
         Returns:
             - X_hat (numpy.ndarray): Combined AR and exogenous features.
-            - Y_hat (numpy.ndarray): Target values as a column vector.
+            - y_hat (numpy.ndarray): Target values as a column vector.
         """
-
-        self.N_AR = N_AR
 
         # Initialise auto-regressive features, exogenous features and targets
         ar_features, exog_features, targets = [], [], []
 
         # Create ARX data-matrix
-        for t in range(self.N_AR, len(Y)):
-            ar_features.append(Y[t-self.N_AR:t])     # AR terms
+        for t in range(N_AR, len(y)):
+            ar_features.append(y[t-N_AR:t])     # AR terms
             exog_features.append(X[t])       # Exogenous inputs
-            targets.append(Y[t])             # Target value
+            targets.append(y[t])             # Target value
         X_hat = np.hstack([exog_features, ar_features])
-        Y_hat = np.array(targets)
+        y_hat = np.array(targets)
 
-        return X_hat, Y_hat
-    
+        return X_hat, y_hat
+
+    def train(self, X, y, theta, sigma, N_AR, learning_rate=0.001, N_itt=10):
+        # Doesn't tune sigma at the moment
+
+        X_AR, y_AR = self._prepare_arx_data(X=X, y=y, N_AR=N_AR)
+
+        for i in range(N_itt):
+            t0 = time.time()
+            g = self.dlogp(X=X_AR, y=y_AR, theta=theta, sigma=sigma)
+            dt = time.time() - t0
+            theta = theta + learning_rate * g
+            print("Iteration", i, "theta = ", theta, "gradient evaluation took", dt, "seconds")
+
+        self.set_hyperparameters(X=X, y=y, theta=theta, sigma=sigma, N_AR=N_AR)
+        print("Finished training")
+
     def set_hyperparameters(self, X, y, theta, sigma, N_AR):
+        self.N_AR = N_AR
         X_AR, y_AR = self._prepare_arx_data(X, y, N_AR)
         super().set_hyperparameters(X=X_AR, y=y_AR, theta=theta, sigma=sigma)
 
--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="lazy_GP",  # Replace with your project name
-    version="1.2.0",
-    packages=find_packages(),  # Automatically finds all packages in your Python directory
-    install_requires=[],  # List dependencies here, e.g., ["numpy", "requests"]
+    name="lazy_GP",
+    version="1.2.1",
+    packages=find_packages(),
+    install_requires=[],
     entry_points={
         "console_scripts": [
-            # Define any command-line scripts here if needed
-            # Example: "my_command = my_package.module:main_function"
         ],
     },
 )
